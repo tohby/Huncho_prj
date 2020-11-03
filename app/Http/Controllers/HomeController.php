cdfd1c14945ee85d5d483f288ef76b6d4c0d8967
tadeyinka <tohbyyy@gmail.com>
2020-11-03 13:30:15 +0700

    Landing page carousel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,19 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     /**
      * Show the application dashboard.
      *
@@ -23,6 +14,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $products = Product::orderBy('created_at', 'desc')->get();
+        return view('welcome')->with('products', $products);
     }
 }
