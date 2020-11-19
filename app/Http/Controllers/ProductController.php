6f9eb71d7ca8912abe4a72cb39422633cb8f9402
tadeyinka <tohbyyy@gmail.com>
2020-11-19 09:42:20 +0700

    Update product validation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -39,7 +39,7 @@ class ProductController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'name' => 'required',
+            'name' => 'required:unique:products',
             'desc' => 'required',
             'color' => 'required',
             'transmission' => 'required',
